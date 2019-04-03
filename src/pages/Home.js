90d4904e07def61c89cf2a9281e5cc46f71df663
gagansaini1212 <gagansaini1212@gmail.com>
2019-04-03 15:41:34 +0530

    Scss file created for bulma

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,10 +3,9 @@ import styled from 'styled-components';
 
 import Layout from '../components/Layout';
 import Seo from '../components/Seo';
-import InputFiled from '../components/home/InputField';
+import InputFiled from '../components/InputField';
 import WatchTransaction from '../components/home/WatchTransaction';
-import Button from '../components/home/Button';
-import theme from '../utils/theme';
+import Button from '../components/Button';
 
 const Header = styled.h1`
   font-size: ${props => props.theme.headerFont};
@@ -19,8 +18,8 @@ export default () => (
     <section className="section">
       <div className="container">
         <Header className="title has-text-weight-semibold">Create trigger</Header>
-        <InputFiled labelText="name" />
         <WatchTransaction />
+        <InputFiled labelText="name" />
         <InputFiled labelText="from address" />
         <InputFiled labelText="to address" />
         <Button buttonText="NEXT" bgColor="red" width="100%" height="51px" />
